a76d9b76732a78f3365713dfef852d7ed6a6f6c3
Trevor Seitz <trevinla@gmail.com>
2018-06-04 19:18:49 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,59 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Lanmark.all
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks' do
+#create New Landmark
+    @landmark = Landmark.new(params[@landmark])
+
+#assign figure if needed
+    if !params[:figures][:name].empty?
+      @landmarks.figures << Figures.find_or_create_by(params[:figures])
+    end
+
+#assign title if needed
+    if !params[:title][:name].empty?
+      @landmarks.titles << Title.find_or_create_by(params[:title])
+    end
+
+    @landmark.save
+
+    redirect to "landmarks/#{@landmark.id}"
+  end
+
+  patch '/landmark/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    if !params[:figures][:name].empty?
+      @landmarks.figures << Figure.find_or_create_by(params[:figure])
+    end
+    if !params[:title][:name].empty?
+      @landmarks.titles << Title.find_or_create_by(params[:title])
+    end
+
+    @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
