476d67a270d2334841dfa6a6ee8b5369d2eb6e2d
Trevor Seitz <trevinla@gmail.com>
2018-06-05 13:39:08 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,30 +10,17 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/new'
   end
 
-  get '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-
-    erb :'/figures/show'
-  end
-
-  get '/landmarks/:id/edit' do
-    @landmark = Landmark.find_by_id(params[:id])
-
-    erb :'landmarks/edit'
-  end
-
   post '/landmarks' do
 #create New Landmark
-    @landmark = Landmark.new(params[@landmark])
-
+    @landmark = Landmark.create(params["landmark"])
 #assign figure if needed
-    if !params[:figures][:name].empty?
-      @landmarks.figures << Figures.find_or_create_by(params[:figures])
+    if !params[:figure][:name].empty?
+      @landmark.figures << Figure.create(params[:figures])
     end
 
 #assign title if needed
     if !params[:title][:name].empty?
-      @landmarks.titles << Title.find_or_create_by(params[:title])
+      @landmark.titles << Title.create(params[:title])
     end
 
     @landmark.save
@@ -41,14 +28,26 @@ class LandmarksController < ApplicationController
     redirect to "landmarks/#{@landmark.id}"
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'landmarks/edit'
+  end
+
   patch '/landmark/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
+    @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
     if !params[:figures][:name].empty?
-      @landmarks.figures << Figure.find_or_create_by(params[:figure])
+      @landmark.figures << Figure.create(params[:figure])
     end
     if !params[:title][:name].empty?
-      @landmarks.titles << Title.find_or_create_by(params[:title])
+      @landmark.titles << Title.create(params[:title])
     end
 
     @landmark.save
