5b544f70613d5602156c29f52cefea938459dabb
Trevor Seitz <trevinla@gmail.com>
2018-06-05 15:20:43 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -40,7 +40,7 @@ class LandmarksController < ApplicationController
     erb :'landmarks/edit'
   end
 
-  patch '/landmark/:id' do
+  post '/landmark/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
     if !params[:figures][:name].empty?
