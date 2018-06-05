9267f4308b574c4c5d04748cb6ece1efd483df2f
Trevor Seitz <trevinla@gmail.com>
2018-06-05 17:39:43 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -43,7 +43,7 @@ class LandmarksController < ApplicationController
   post '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
-    if !params[:figures].empty?
+    if !params[:figures][:name].empty?
       @landmark.figures << Figure.create(params[:figure])
     end
     if !params[:title][:name].empty?
